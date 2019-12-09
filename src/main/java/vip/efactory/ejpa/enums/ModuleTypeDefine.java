76b54d778b793245ad33ecaafc6fd5c6d3261a4d
dbdu <1797890817@qq.com>
2019-12-09 14:36:28 +0800

    fix bug

--- src/main/java/vip/efactory/ejpa/enums/ModuleTypeDefine.java
+++ src/main/java/vip/efactory/ejpa/enums/ModuleTypeDefine.java
@@ -4,6 +4,6 @@ package vip.efactory.ejpa.enums;
  * 定义模块的首部代码
  */
 public interface ModuleTypeDefine {
-    int COMMON = 1000;          // 通用模块
-    int DATABASE = 2000;        // 数据库模块
+    int COMMON = 0;          // 通用模块
+    int DATABASE = 1000;        // 数据库模块
 }
