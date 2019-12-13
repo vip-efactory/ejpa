044213a16456a61ecf7fbd5182bbc7759be32818
dbdu <1797890817@qq.com>
2019-12-13 09:45:53 +0800

    高级搜索更新计划

--- src/main/java/vip/efactory/ejpa/base/entity/BaseSearchField.java
+++ src/main/java/vip/efactory/ejpa/base/entity/BaseSearchField.java
@@ -44,6 +44,39 @@ public class BaseSearchField {
      */
     @ApiModelProperty(value = "结束值", name = "val2", notes = "搜索类型为０或１时此值不用，为２范围查询时，此值是结束值")
     private String val2;
+
+    /**
+     * 条件级别，1级，2级...  1级条件就是最外层条件，若不写默认为第一层
+     */
+    @ApiModelProperty(value = "条件层次", name = "level", notes = "条件级别，1级，2级...  1级条件就是最外层条件，")
+    private String level;
+
+    /**
+     * 条件位置顺序，例如都是同级，一级的情况下，这个条件是在前面还是后面。可不写，有则使用，没有则随机排
+     */
+    @ApiModelProperty(value = "条件位置顺序", name = "order", notes = "条件级别，1级，2级...  1级条件就是最外层条件，")
+    private Integer order;
+
+    /**
+     * 逻辑类型,当前条件是与的关系还是或的关系，0 为或的关系，1为与的关系   若为空，默认为0
+     */
+    @ApiModelProperty(value = "条件逻辑类型", name = "logicalType", notes = "当前条件是与的关系还是或的关系，0 为或的关系，1为与的关系；若为空，默认为0")
+    private Integer logicalType;
+
+    /**
+     * 括号组，哪些条件在同一个组里，例如：（a=3 || b=4） && （c=5 || d=7），简单条件时允许为空
+     */
+    @ApiModelProperty(value = "括号组", name = "bracketsGroup", notes = "哪些条件在同一个组里，例如：（a=3 || b=4） && （c=5 || d=7），简单条件时允许为空")
+    private String bracketsGroup;
+
+    /**
+     * 组逻辑类型,当前组条件是与的关系还是或的关系，0 为或的关系，1为与的关系   若为空，默认为0
+     *  是||（a=3 || b=4）
+     *  还是 &&（c=5 || d=7）
+     */
+    @ApiModelProperty(value = "组逻辑类型", name = "logicalTypeGroup", notes = "当前组条件是与的关系还是或的关系，0 为或的关系，1为与的关系；若为空，默认为0")
+    private Integer logicalTypeGroup;
+
 //
 //    /**
 //     * name名称对应的字段的数据类型，例如：String,Integer,Date,Number等,JPA实现between查询必须要有
